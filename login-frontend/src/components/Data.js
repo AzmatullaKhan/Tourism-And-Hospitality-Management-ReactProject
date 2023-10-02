1de6056009d05848cbedca343888be3774eeae11
klu_2200031618 <2200031618@gmail.com>
2023-10-02 19:18:21 +0530

    initial commit

---
+++ login-frontend/src/components/Data.js
@@ -0,0 +1,18 @@
+const trialElements = [
+    {id: 1,  pic: require("./images/azmatulla.jpg") },
+    {id: 2,  pic: require("./images/balaji.jpg")},
+    {id: 3,  pic: require("./images/santhosh.jpg") },
+    {id: 4,  pic: require("./images/azmatulla.jpg")},
+
+    {id: 5,  pic: require("./images/azmatulla.jpg") },
+    {id: 6,  pic: require("./images/santhosh.jpg")},
+    {id: 7,  pic: require("./images/balaji.jpg") },
+    {id: 8,  pic: require("./images/azmatulla.jpg")},
+    {id: 9,  pic: require("./images/santhosh.jpg") },
+    {id: 10,  pic: require("./images/balaji.jpg")},
+    {id: 11,  pic: require("./images/azmatulla.jpg") },
+    {id: 12,  pic: require("./images/santhosh.jpg") }, 
+    {id: 13,  pic: require("./images/balaji.jpg")}, 
+    ]
+
+export default trialElements;
--- login-frontend/src/components/Home.js
+++ login-frontend/src/components/Home.js
@@ -1,8 +1,15 @@
 // import { useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 import { getData, handleLogout, isLoggedIn } from "./Login"
+import trialElements from "./Data"
 import './zcss.css'
+import { useRef } from "react"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 export const Home = () =>{
+    const sliderTrail1Ref = useRef(null);
+    const sliderTrail2Ref = useRef(null);
+    const scrollAmount = 256;
     function navFunction() {
         var x = document.getElementById("navbarr");
         var y = document.getElementById("nav");
@@ -36,23 +43,76 @@ export const Home = () =>{
             <div className="home-main">
                 <div className="navbar" id="navbarr">
                     <button className="home-button">LOGO</button>
-                        <div>
-                            
-                            <button className="home-button glow" onClick={handleBook}>Book</button>
-                            <button className="home-button glow">help</button>
-                            {
-                                isLoggedIn?
-                                (<button className="home-button glow" onClick={handleLogout}>LogOut</button>):
-                                (<button className="home-button glow" onClick={handleLogin}>Login</button>)
-                            }
-                            {
-                                isLoggedIn? 
-                                (<button className="home-button glow" onClick={handleData}>Profile</button>):
-                                (<button className="home-button glow" onClick={handleSignIn}>SignIn</button>)
-                            }
-                            <button className="home-button glow" onClick={handleAbout}>about</button>
-                            <button className="home-button glow icon" id="nav" onClick={navFunction}>click</button>
+                    <div>
+                        <button className="home-button glow" onClick={handleBook}>Book</button>
+                        <button className="home-button glow">help</button>
+                        {
+                            isLoggedIn?
+                            (<button className="home-button glow" onClick={handleLogout}>LogOut</button>):
+                            (<button className="home-button glow" onClick={handleLogin}>Login</button>)
+                        }
+                        {
+                            isLoggedIn? 
+                            (<button className="home-button glow" onClick={handleData}>Profile</button>):
+                            (<button className="home-button glow" onClick={handleSignIn}>SignIn</button>)
+                        }
+                        <button className="home-button glow" onClick={handleAbout}>about</button>
+                        <button className="home-button glow icon" id="nav" onClick={navFunction}>click</button>
+                    </div>
+                </div>
+                <div className="state-main">
+                    <center><h1>Trial1</h1></center>
+                    <div className="state-holder-main">
+                        <button className="nav-btn"
+                            onClick={() => {
+                            const container = sliderTrail1Ref.current;
+                            container.scrollLeft -= scrollAmount; // Scroll left by the specified amount
+                            }}
+                            >
+                            <ChevronLeftIcon />
+                        </button>
+                        <div className="state-holder-mini" ref={sliderTrail1Ref}>
+                            {trialElements.map((element) => (
+                            <img src={element.pic} alt={element.id} className="image"/>
+                            ))}
                         </div>
+                        
+                        <button className="nav-btn"
+                            onClick={() => {
+                            const container = sliderTrail1Ref.current;
+                            container.scrollLeft += scrollAmount; // Scroll left by the specified amount
+                            }}
+                            >
+                            <ChevronRightIcon />
+                        </button>
+                    </div>
+                </div>
+                <div className="state-main">
+                    <center><h1>Trial2</h1></center>
+                    <div className="state-holder-main">
+                        <button className="nav-btn"
+                            onClick={() => {
+                            const container = sliderTrail2Ref.current;
+                            container.scrollLeft -= scrollAmount; // Scroll left by the specified amount
+                            }}
+                            >
+                            <ChevronLeftIcon />
+                        </button>
+                        <div className="state-holder-mini" ref={sliderTrail2Ref}>
+                            {trialElements.map((element) => (
+                            <img src={element.pic} alt={element.id} className="image"/>
+                            ))}
+                        </div>
+                        
+                        <button className="nav-btn"
+                            onClick={() => {
+                            const container = sliderTrail2Ref.current;
+                            container.scrollLeft += scrollAmount; // Scroll left by the specified amount
+                            }}
+                            >
+                            <ChevronRightIcon />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
