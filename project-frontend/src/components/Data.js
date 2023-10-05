a304ab5908059a82456ca0f2bd6aa00c260afbfe
AzmatullaKhan <kk258786@gmail.com>
2023-10-05 21:45:16 +0530

    Hidden desc page has been set for individual component

--- project-frontend/src/components/Data.js
+++ project-frontend/src/components/Data.js
@@ -1,20 +1,20 @@
 const trialElements = [
-    {id: 1,  pic: require("./images/popular1.jpg") },
-    {id: 2,  pic: require("./images/popular2.jpg")},
-    {id: 3,  pic: require("./images/popular3.jpg") },
-    {id: 4,  pic: require("./images/popular1.jpg") },
-    {id: 5,  pic: require("./images/popular3.jpg")},
-    {id: 6,  pic: require("./images/popular2.jpg") },
-    {id: 7,  pic: require("./images/popular1.jpg")},
-    {id: 8,  pic: require("./images/popular3.jpg") },
-    {id: 9,  pic: require("./images/popular2.jpg")},
-    {id: 10,  pic: require("./images/popular1.jpg") },
+    {id: 1,  pic: require("./images/popular1.jpg"), desc:"Trial" },
+    {id: 2,  pic: require("./images/popular2.jpg"), desc:"Trial"},
+    {id: 3,  pic: require("./images/popular3.jpg"), desc:"Trial" },
+    {id: 4,  pic: require("./images/popular1.jpg"), desc:"Trial" },
+    {id: 5,  pic: require("./images/popular3.jpg"), desc:"Trial"},
+    {id: 6,  pic: require("./images/popular2.jpg"), desc:"Trial" },
+    {id: 7,  pic: require("./images/popular1.jpg"), desc:"Trial"},
+    {id: 8,  pic: require("./images/popular3.jpg"), desc:"Trial" },
+    {id: 9,  pic: require("./images/popular2.jpg"), desc:"Trial"},
+    {id: 10,  pic: require("./images/popular1.jpg"), desc:"Trial" },
     ]
 
 export default trialElements;
 
 export const carouselElements=[
-    {id: 1,  pic: require("./images/popular1.jpg") },
+    {id: 1,  pic: require("./images/popular1.jpg")},
     {id: 2,  pic: require("./images/popular2.jpg")},
     {id: 3,  pic: require("./images/popular3.jpg") }
 ]
