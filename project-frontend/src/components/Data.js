0aebd0bb58d855a0b15d2b2c369182f165b48bb8
DonthineniSanthosh <2200031630@kluniversity.in>
2023-10-14 23:43:10 +0530

    Committing changes in data component

--- project-frontend/src/components/Data.js
+++ project-frontend/src/components/Data.js
@@ -1,31 +1,54 @@
 const AndhraPradesh = [
-    {id: 1, pic: require("./images/popular1.jpg"),placeName:"Guntur", innerHotels:[
-        {id:1,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trail1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1", url:require("./images/popular1.jpg")},
-        {id:2,pic:"./images/popular2.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:3,pic:"./images/popular3.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:6,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:4,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:7,pic:"./images/popular2.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:8,pic:"./images/popular3.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:9,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"}
+    {id: 1, pic: require("./images/guntur/gun_0.png"),placeName:"guntur", innerHotels:[
+        {id:1,pic:"./images/guntur/gun_1.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"guntur", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1", url:require("./images/popular1.jpg")},
+        {id:2,pic:"./images/guntur/gun_2.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:3,pic:"./images/guntur/gun_3.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:6,pic:"./images/guntur/gun_4.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:4,pic:"./images/guntur/gun_5.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:7,pic:"./images/guntur/gun_6.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:8,pic:"./images/guntur/gun_7.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:9,pic:"./images/guntur/gun_8.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"}
     ]},
 
 
-    {id: 2,  pic: require("./images/popular2.jpg"), placeName:"Vijayawada",innerHotels:[
-        {id:1,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1", url:require("./images/popular1.jpg")},
-        {id:2,pic:"./images/popular2.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:3,pic:"./images/popular3.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:6,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:4,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:7,pic:"./images/popular2.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:8,pic:"./images/popular3.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
-        {id:9,pic:"./images/popular1.jpg", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"}
+    {id: 2,  pic: require("./images/andhra_pradesh/AP_VJY0.png"), placeName:"Vijayawada",innerHotels:[
+        {id:1,pic:"./images/andhra_pradesh/AP_VJY1.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"vijayawada", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1", url:require("./images/popular1.jpg")},
+        {id:2,pic:"./images/andhra_pradesh/AP_VJY2.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:3,pic:"./images/andhra_pradesh/AP_VJY3.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:6,pic:"./images/andhra_pradesh/AP_VJY4.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:4,pic:"./images/andhra_pradesh/AP_VJY5.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:7,pic:"./images/andhra_pradesh/AP_VJY6.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:8,pic:"./images/andhra_pradesh/AP_VJY7.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:9,pic:"./images/andhra_pradesh/AP_VJY8.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"}
+    ]},
+
+   
+
+
+    {id: 3,  pic: require("./images/vizag/vizag_0.png"),placeName:"Vizag",innerHotels:[
+        {id:1,pic:"./images/andhra_pradesh/AP_VJY1.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"vizag", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1", url:require("./images/popular1.jpg")},
+        {id:2,pic:"./images/andhra_pradesh/AP_VJY2.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:3,pic:"./images/andhra_pradesh/AP_VJY3.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:6,pic:"./images/andhra_pradesh/AP_VJY4.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:4,pic:"./images/andhra_pradesh/AP_VJY5.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:7,pic:"./images/andhra_pradesh/AP_VJY6.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:8,pic:"./images/andhra_pradesh/AP_VJY7.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:9,pic:"./images/andhra_pradesh/AP_VJY8.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"}
     ]},
 
 
 
-    {id: 3,  pic: require("./images/popular3.jpg"), desc:"Trial3", nameHotel:"Trial3", placeHotel:"Trial3", stateHotel:"Trial3", totalAvailableRoomsHotel:"Trial3", costPerNightHotel:"25000", ratingHotel:"Trial3" },
-    {id: 4,  pic: require("./images/popular1.jpg"), desc:"Trial4", nameHotel:"Trial4", placeHotel:"Trial4", stateHotel:"Trial4", totalAvailableRoomsHotel:"Trial4", costPerNightHotel:"25000", ratingHotel:"Trial4" },
+
+    {id: 4,  pic: require("./images/tirupathi/tir_0.png"),placeName:"tirupathi",innerHotels:[
+        {id:1,pic:"./images/andhra_pradesh/AP_VJY1.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"vizag", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1", url:require("./images/popular1.jpg")},
+        {id:2,pic:"./images/andhra_pradesh/AP_VJY2.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:3,pic:"./images/andhra_pradesh/AP_VJY3.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:6,pic:"./images/andhra_pradesh/AP_VJY4.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:4,pic:"./images/andhra_pradesh/AP_VJY5.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:7,pic:"./images/andhra_pradesh/AP_VJY6.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:8,pic:"./images/andhra_pradesh/AP_VJY7.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"},
+        {id:9,pic:"./images/andhra_pradesh/AP_VJY8.png", desc:"Trial1", nameHotel:"Trial1", placeHotel:"Trial1", stateHotel:"Trial1", totalAvailableRoomsHotel:"Trial1", costPerNightHotel:"25000", ratingHotel:"Trial1"}
+    ]},
     {id: 5,  pic: require("./images/popular3.jpg"), desc:"Trial5", nameHotel:"Trial5", placeHotel:"Trial5", stateHotel:"Trial5", totalAvailableRoomsHotel:"Trial5", costPerNightHotel:"25000", ratingHotel:"Trial5" },
 ]
 export default AndhraPradesh;
